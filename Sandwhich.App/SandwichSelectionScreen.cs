62f3a9d50ecef80d2e53ba7c7bd00a3653a2bac3
Nathan Gonzalez <nathan@copypastedeveloper.com>
2018-02-06 21:32:15 -0600

    adding initial testing for my sandwich creation app

---
+++ Sandwhich.App/SandwichSelectionScreen.cs
@@ -0,0 +1,28 @@
+﻿namespace Sandwhich.App
+{
+    public class SandwichSelectionScreen
+    {
+        readonly Sandwich _sandwich = new Sandwich();
+
+        public void AddTomatoes(int numberOfTomatoes)
+        {
+            _sandwich.Tomatoes += numberOfTomatoes;
+        }
+
+        public Sandwich BuildSandwich()
+        {
+            return _sandwich;
+        }
+
+        public void AddOnions()
+        {
+            _sandwich.Onions = true;
+        }
+    }
+
+    public class Sandwich
+    {
+        public int Tomatoes { get; set; }
+        public bool Onions { get; set; }
+    }
+}
---
+++ Sandwhich.Tests/Class1.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using Sandwhich.App;
+
+namespace Sandwhich.Tests
+{
+    [TestFixture]
+    public class Adding_Toppings
+    {
+        [Test]
+        public void Clicking_Add_Tomatoes_Adds_Tomatoes_To_My_Order()
+        {
+            //Arrange
+            var screen = new SandwichSelectionScreen();
+
+            //Act
+            screen.AddTomatoes(2);
+            var sandwich = screen.BuildSandwich();
+
+            //Assert
+            Assert.AreEqual(2,sandwich.Tomatoes);
+        }
+
+        [Test]
+        public void Clicking_Add_Tomatoes_Twice_Adds_More_Tomatoes_To_My_Order()
+        {
+            //Arrange
+            var screen = new SandwichSelectionScreen();
+
+            //Act
+            screen.AddTomatoes(2);
+            screen.AddTomatoes(2);
+            var sandwich = screen.BuildSandwich();
+
+            //Assert
+            Assert.AreEqual(4,sandwich.Tomatoes);
+        }
+
+        [Test]
+        public void Clicking_Add_Onions_Adds_Onions_To_My_Order()
+        {
+            //Arrange
+            var screen = new SandwichSelectionScreen();
+
+            //Act
+            screen.AddOnions();
+            var sandwich = screen.BuildSandwich();
+
+            //Assert
+            Assert.IsTrue(sandwich.Onions);
+        }
+    }
+}
